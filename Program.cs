1885971a67ebda8d503673ccc86f0d9b8dca13f4
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-09 19:15:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Prog120_W24_L2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // c w tab to quickly create a console writeline
+
+            // variables
+            // string - is a data type for characters. abc, 123, ^&$
+            // a string is whatever is in double quotes ""
+            // Declaring and initializing a string
+
+            Console.WriteLine("Hello, World");
+
+        } // main
+
+    } // class
+
+} // 
+
+// To Comment do double forward slash
+// The hotkey is Ctrl + k, c
+// To uncomment do Ctrl + k, u
